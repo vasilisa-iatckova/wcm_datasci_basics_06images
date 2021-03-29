f6b7addd675ab5226d50c48bb6e25a29931c86d1
LI <yatskova.anna@gmail.com>
2021-03-29 16:27:17 -0400

    Got get_data working

--- assignment/functions/utils.py
+++ assignment/functions/utils.py
@@ -1,3 +1,20 @@
 # Utility functions for processing .csv data in assignment 6
 
+# imports
+import natsort  
+import os
+import glob
+
 # get_files function:
+def get_files(path_pattern):
+    """
+    Extracts file in alphanumerical order that match the provided pattern
+    """
+    if isinstance(path_pattern, list):
+        path_pattern = os.path.join(*path_pattern)
+        
+    files = natsort.natsorted(glob.glob(path_pattern))
+    if not files:
+        raise FileNotFoundError('Pattern could not detect file(s)')
+        
+    return files
